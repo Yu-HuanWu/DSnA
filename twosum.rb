01840db1d8c3e8289b72a6d0ed8b203838a599cc
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-07 15:15:37 -0800

    finished two sum

--- twosum.rb
+++ twosum.rb
@@ -1,3 +1,14 @@
 def two_sum(nums, target)
-    
+    i= 0
+    while i < nums.length-1
+        j= i+1;
+        while j < nums.length
+            if nums[i] + nums[j] == target
+                return [i, j]
+            else
+                j+=1
+            end
+        end
+        i+=1
+    end
 end
