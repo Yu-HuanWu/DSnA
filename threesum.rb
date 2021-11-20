787f3d3cde2204e83b411e0b885510407b6eb5e0
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-20 14:35:33 -0800

    threesum but time limit exceeded

--- threesum.rb
+++ threesum.rb
@@ -20,5 +20,25 @@
 # -105 <= nums[i] <= 105
 
 def three_sum(nums)
-    nums.
+    return [] if nums.length < 3
+    result= []
+    i= 0
+    while i < nums.length-2
+        j= i+1;
+        while j < nums.length-1
+            k= j+1
+            while k < nums.length
+                if nums[i] + nums[j] + nums[k] == 0
+                    ans= [nums[i], nums[j], nums[k]].sort
+                    result<< ans if !result.include?(ans)
+                end
+                k+=1
+            end
+            j+=1
+        end
+        i+=1
+    end
+    return result
 end
+
+p three_sum([-1,0,1,2,-1,-4])
