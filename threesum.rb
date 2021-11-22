ca6bd8ee403510d71478da15a4f0a9e077be4119
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-21 16:11:59 -0800

    somehow this threesum worked

--- threesum.rb
+++ threesum.rb
@@ -20,25 +20,58 @@
 # -105 <= nums[i] <= 105
 
 def three_sum(nums)
+    # return [] if nums.length < 3
+    # result= []
+    # i= 0
+    # while i < nums.length-2
+    #     j= i+1;
+    #     while j < nums.length-1
+    #         k= j+1
+    #         while k < nums.length
+    #             if nums[i] + nums[j] + nums[k] == 0
+    #                 ans= [nums[i], nums[j], nums[k]].sort
+    #                 result<< ans if !result.include?(ans)
+    #             end
+    #             k+=1
+    #         end
+    #         j+=1
+    #     end
+    #     i+=1
+    # end
+    # return result
+
     return [] if nums.length < 3
+    nums= nums.sort
     result= []
     i= 0
     while i < nums.length-2
-        j= i+1;
-        while j < nums.length-1
-            k= j+1
-            while k < nums.length
-                if nums[i] + nums[j] + nums[k] == 0
-                    ans= [nums[i], nums[j], nums[k]].sort
-                    result<< ans if !result.include?(ans)
+        break if nums[i] > 0
+        # if i > 0 && nums[i] == nums[i-1] 
+            j= i+1;
+            k= (nums.length) -1
+            while j < k
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == 0
+                    result << [nums[i], nums[j], nums[k]]
+                    while nums[j]== nums[j+1]
+                        j+=1
+                    end
+
+                    while nums[k] == nums[k-1]
+                        k-=1
+                    end
+                    j+=1
+                    k-=1
+                elsif sum < 0
+                    j+=1
+                else
+                    k-=1
                 end
-                k+=1
             end
-            j+=1
-        end
-        i+=1
+        # end
+    i+=1
     end
-    return result
+    return result.uniq
 end
 
 p three_sum([-1,0,1,2,-1,-4])
