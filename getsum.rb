5bfd6fa2176e408bf66d0661d92e9d3a3b00083c
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-25 12:14:11 -0800

    need to use binary

--- getsum.rb
+++ getsum.rb
@@ -1,14 +1,11 @@
 # Given two integers a and b, return the sum of the two integers without using
 #  the operators + and -.
 
- 
-
 # Example 1:
-
 # Input: a = 1, b = 2
 # Output: 3
-# Example 2:
 
+# Example 2:
 # Input: a = 2, b = 3
 # Output: 5
  
@@ -18,5 +15,18 @@
 # -1000 <= a, b <= 1000
 
 def get_sum(a, b)
-    
+    result=[]
+    if a > 0
+        a.times {|i| result<< "hi"}
+    else
+        a.times {|i| result.pop()}
+    end
+    if b > 0
+        b.times {|i| result<< "hi"}
+    else
+       (-b).times {|i| result.pop}
+    end
+    result.length
 end
+
+p get_sum(1, -4)
