e8d1a6a0a87da95ab8826fa18227e4d3441e79f7
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-28 10:10:33 -0800

    get sum won't work for -1 for some reason

--- getsum.rb
+++ getsum.rb
@@ -15,20 +15,28 @@
 # -1000 <= a, b <= 1000
 
 def get_sum(a, b)
-    result=[]
-    if a > 0
-        a.times {|i| result<< "hi"}
-    else
-        a.times {|i| result.pop()}
+    # result=[]
+    # if a > 0
+    #     a.times {|i| result<< "hi"}
+    # else
+    #     a.times {|i| result.pop()}
+    # end
+    # if b > 0
+    #     b.times {|i| result<< "hi"}
+    # else
+    #    (-b).times {|i| result.pop}
+    # end
+    # result.length
+
+    carry = nil
+    while a != 0 && b != 0
+        carry = ((a & b) << 1);
+        a = a ^ b;
+        b = carry;
     end
-    if b > 0
-        b.times {|i| result<< "hi"}
-    else
-       (-b).times {|i| result.pop}
-    end
-    result.length
+    return a ^ b;
 end
 
 p get_sum(1, -4)
 p get_sum(1, 100)
-p get_sum(100, -100)
+p get_sum(-1, 1)
