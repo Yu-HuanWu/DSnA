632c852592db40e86662f7c6d75e1dbfbc459d41
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-12-03 10:36:48 -0800

    climbstairs similar to fibonaci

--- climbstairs.rb
+++ climbstairs.rb
@@ -21,11 +21,32 @@
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
  
+# input 4 = 5
+# [1,1,1,1] [2,1,1] [1,2,1] [1,1,2] [2, 2]
+
+# input 5 = 8
+# [1, 1, 1, 1, 1] [2, 1, 1, 1] [1, 2, 1, 1] [1, 1, 2, 1] [1, 1, 1, 2]
+# [2, 2, 1] [2, 1, 2] [1, 2, 2]
 
 # Constraints:
 
 # 1 <= n <= 45
 
 def climb_stairs(n)
-    
+    # return 1 if n==1
+    # return 2 if n==2
+    # return 3 if n==3
+
+    # return climb_stairs(n-1)+climb_stairs(n-2)
+    a = 1
+    b = 1
+    n.times { 
+        tempa = a
+        tempb = b
+        a = tempb
+        b = tempa+tempb
+    }
+    a
 end
+
+p climb_stairs(44)
