5b0e14026af22a4d57aa7796455d40accd551b53
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-12-12 22:33:05 -0800

    length_of_lis done but need practice

--- lengthoflis.rb
+++ lengthoflis.rb
@@ -1,7 +1,9 @@
 =begin
 Given an integer array nums, return the length of the longest strictly increasing subsequence.
 
-A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+A subsequence is a sequence that can be derived from an array by deleting some
+ or no elements without changing the order of the remaining elements. For 
+ example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
 
  
 
@@ -22,8 +24,22 @@ Output: 1
 =end
 
 def length_of_lis(nums)
-    
+    list=[]
+    i= 0
+    while i  < nums.length
+        list<< 1
+        j=0
+        while j< i
+            if nums[j]< nums[i]
+                list[i] > list[j]+1 ? list[i] : list[i]= list[j]+1
+            end
+            j+=1
+        end
+        i+=1
+    end
+    list.sort[-1]
 end
 
 p length_of_lis([0,1,0,3,2,3])
+p length_of_lis([10,9,2,5,3,7,101,18])
 p length_of_lis([7,7,7,7,7,7,7])
