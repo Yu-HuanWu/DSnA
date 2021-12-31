291078e6ad098848449e808f44098cedccee2809
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-12-31 00:14:01 -0800

    insert interval

---
+++ insertInterval.js
@@ -0,0 +1,7 @@
+var insert = function (intervals, newInterval) {
+
+};
+
+let intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+let newInterval = [4, 8]
+console.log(insert(intervals, newInterval)) //[[1,2],[3,10],[12,16]]
