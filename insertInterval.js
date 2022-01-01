4684e2034b7cb0618a7e4aa9d2615d3d99e8ae2f
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-01-01 01:15:06 -0800

    unfinished insertInterval

--- insertInterval.js
+++ insertInterval.js
@@ -1,5 +1,15 @@
-var insert = function (intervals, newInterval) {
+function numberRange(start, end) {
+    return new Array(end - start).fill().map((d, i) => i + start);
+}
 
+var insert = function (intervals, newInterval) {
+    let resultArr= [];
+    intervals.forEach(interval=>{
+        if (!numberRange(interval[0], interval[1]).includes(newInterval[0]) && !numberRange(interval[0], interval[1]).includes(newInterval[1])) {
+            resultArr.push(interval)
+        }
+    })
+    return resultArr
 };
 
 let intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
