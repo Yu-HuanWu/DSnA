aaf413bfe8f3e9041fa664bade7a50a8ea0a2032
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-01-17 09:31:04 -0800

    added conditional

--- eraseOverlapIntervals.js
+++ eraseOverlapIntervals.js
@@ -31,7 +31,9 @@ var eraseOverlapIntervals = function (intervals) {
     intervals= intervals.sort((a,b)=> a[0]-b[0])
     let counter;
     for (let i= 0; i< intervals.length-1; i++){
-        intervals[i]
+        if (intervals[i][1] < intervals[i+1][0]){
+            
+        }
     }
     console.log(intervals)
     return counter
